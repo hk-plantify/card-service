b826bfd3d267de8416c24ae20be77110f10c9582
choihoyoon <andychoi1997@naver.com>
2024-12-09 14:31:17 +0900

    feat: update database.py

--- database/database.py
+++ database/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from config.config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine.execute("CREATE DATABASE IF NOT EXISTS `card-db`;")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
