9da13afc25154c49ebd7ae5121b4db1c30141128
choihoyoon <andychoi1997@naver.com>
2024-12-09 13:32:43 +0900

    feat: update config

--- config/config.py
+++ config/config.py
@@ -8,6 +8,14 @@ PASSWORD = os.getenv("PASSWORD")
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
 DBNAME = os.getenv("DBNAME")
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?charset=utf8mb4"
-
 AUTH_SERVICE_URL = os.getenv("AUTH_SERVICE_URL")
+
+print(f"USERNAME: {USERNAME}")
+print(f"PASSWORD: {PASSWORD}")
+print(f"HOST: {HOST}")
+print(f"PORT: {PORT}")
+print(f"DBNAME: {DBNAME}")
+print(f"AUTH_SERVICE_URL: {AUTH_SERVICE_URL}")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?charset=utf8mb4"
+print(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")
