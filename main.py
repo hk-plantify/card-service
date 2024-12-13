3850bde0fa5835716f6041593d39464ed9eae338
choihoyoon <andychoi1997@naver.com>
2024-12-13 21:23:23 +0900

    feat: add CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database.database import Base, engine
 from domain.mycard.routes import mycard_router as mycard_router
 from domain.mycard.routes import card_router as card_router
@@ -6,6 +7,15 @@ from domain.mycard.routes import card_router as card_router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서 요청 허용 (필요시 특정 도메인으로 제한 가능)
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 app.include_router(mycard_router)
 app.include_router(card_router)
